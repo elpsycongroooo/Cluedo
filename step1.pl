e631f533452c4591302fcaf91934d68507178fe3
Andrew VALLETEAU <avallete@e2r9p2.42.fr>
2015-03-28 12:48:57 +0100

    step

--- step1.pl
+++ step1.pl
@@ -7,18 +7,24 @@ mari_de(jules, lisa).
 mari_de(leon, sylvie).
 mari_de(loic, eve).
 mari_de(paul, julie).
-enfant_de(marc, jean).
-enfant_de(marc, jules).
-enfant_de(marc, leon).
-enfant_de(luc, lisa).
-enfant_de(luc, loic).
-enfant_de(luc, gerard).
-enfant_de(jules, jacques).
-enfant_de(leon, herve).
-enfant_de(leon, julie).
-enfant_de(loic, paul).
-enfant_de(loic, valerie).
-enfant_de(X, Y) :- femme_de(X, M), enfant_de(M, Y).
-femme_de(X, Y) :- mari_de(Y, X).
-beaupere_de(X, Y) :- mari_de(Y, J), enfant_de(X, J), homme(X).
-bellemere_de(X, Y) :- femme_de(Y, J), enfant_de(X, J), femme(X).
+enfant_de(jean, marc).
+enfant_de(jules, marc).
+enfant_de(leon, marc).
+enfant_de(lisa, luc).
+enfant_de(loic, luc).
+enfant_de(gerard, luc).
+enfant_de(jacques, jules).
+enfant_de(herve, leon).
+enfant_de(julie, leon).
+enfant_de(paul, loic).
+enfant_de(valerie, loic).
+
+/* definition des regles */
+enfant_de(E, P) :- femme_de(P, M), enfant_de(E, M).
+femme_de(F, M) :- mari_de(M, F).
+beaupere_de(Bp, Bf) :- homme(Bp), homme(Bf), mari_de(Bf, J), enfant_de(J, Bp).
+beaupere_de(Bp, Bf) :- homme(Bp), femme(Bf), femme_de(Bf, J), enfant_de(J, Bp).
+bellemere_de(Bm, Bf) :- femme(Bm), femme(Bf), femme_de(Bf, J), enfant_de(J, Bm).
+bellemere_de(Bm, Bf) :- femme(Bm), homme(Bf), mari_de(Bf, J), enfant_de(J, Bm).
+ancetre_de(A, D) :- enfant_de(D, A).
+ancetre_de(A, X) :- enfant_de(X, T), ancetre_de(A, T).
